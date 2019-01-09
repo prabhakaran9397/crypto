7f30718e15e3bf0563a535dbac6b288b57817ae4
prabhakaran9397 <prabhakaran9397@gmail.com>
2019-01-09 23:51:00 +0530

    Renamed files

--- set1/detect_single-character_xor.sh
+++ set1/detect_single-character_xor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `cat 4.txt`; do ./single-byte_XOR_cipher.sh $i; done > guesses_for_4.txt
+for i in `cat 4.txt`; do ./single-byte_xor_cipher.sh $i; done > guesses_for_4.txt
 
 grep -E "^[a-z A-Z]*$" guesses_for_4.txt | grep '\S' | less
 
